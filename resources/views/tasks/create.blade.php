8b18b385d19026a86daaece37b9962e2d20200d4
t-1kawa <aaa@gmail.com>
2018-05-25 05:59:59 +0000

    modified edit.blade.php

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -2,13 +2,13 @@
 
 @section('content')
 
-    <h1>タスクを追加する</h1>
+    
     
     
     <div class="row">
-        <div class="col-xs-12">
-        <div class="col-offset-2 col-md-8">
-        <div class="col-offset-3 col-lg-6">
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-lg-offset-3 col-lg-6">
+            <h1>タスクを追加する</h1>
+            
             {!! Form::model($task, ['route' => 'tasks.store']) !!}
             
                 <div class="form-group">
@@ -24,8 +24,6 @@
                 {!! Form::submit('投稿') !!}
             
             {!! Form::close() !!}
-        </div>
-        </div>
         </div>
     </div>
 @endsection
